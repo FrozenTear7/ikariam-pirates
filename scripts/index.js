1872d105d183189c5d68581a0958125bf0c14494
Paweł Mendroch <pawelmendroch7@gmail.com>
2023-10-08 23:24:33 +0200

    DINGDONG

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
 setInterval(() => {
-  //   new Audio(chrome.runtime.getURL('dingdong.mp3')).play()
-
   const boardingButton = document.querySelector(
     '#pirateCaptureBox > div.content > table > tbody > tr:nth-child(2) > td.action > a'
   )
+  const captchaImage = document.querySelector(
+    '#pirateCaptureBox > div.content > form > img'
+  )
 
-  if (boardingButton?.className === 'button capture') {
+  if (captchaImage) {
+    new Audio(chrome.runtime.getURL('dingdong.mp3')).play()
+  } else if (boardingButton?.className === 'button capture') {
     boardingButton.click()
   }
 }, 5000 + Math.random() * 10000)
